95913b1a6d0483244e29ac0e8b8be77e965c1e79
nacarney <nacarney@tcd.ie>
2020-10-26 11:30:26 +0000

    Re-Entered ToString Method

--- main.py
+++ main.py
@@ -31,7 +31,32 @@ def lca(root, n1, n2):
     return root
 
 
-# Driver program to test above function
+def treeToString(root: Node, string: list):
+    # base case
+    if root is None:
+        return
+
+    # push the root data as character
+    string.append(str(root.data))
+
+    # if leaf node, then return
+    if not root.left and not root.right:
+        return
+
+    # for left subtree
+    string.append('(')
+    treeToString(root.left, string)
+    string.append(')')
+
+    # only if right child is present to
+    # avoid extra parenthesis
+    if root.right:
+        string.append('(')
+        treeToString(root.right, string)
+        string.append(')')
+
+
+    # Driver program to test above function
 
 # Let us construct the BST shown in the figure
 root = Node(20)
@@ -41,6 +66,10 @@ root.left.left = Node(4)
 root.left.right = Node(12)
 root.left.right.left = Node(10)
 root.left.right.right = Node(14)
+
+string = []
+treeToString(root, string)
+print(''.join(string))
 
 n1 = 10;
 n2 = 14
