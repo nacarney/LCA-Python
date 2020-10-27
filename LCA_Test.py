63cb36343dea70558cc00ff232bbeec0d0539437
nacarney <nacarney@tcd.ie>
2020-10-27 17:47:50 +0000

    added out of order tree test

--- LCA_Test.py
+++ LCA_Test.py
@@ -30,8 +30,6 @@ class TestLCA(unittest.TestCase):
         n2 = 13
         assert lca(root, n1, n2).data == 15
 
-
-class toString(unittest.TestCase):
     def test_tree_to_string(self):
         root = Node(20)
         root.left = Node(8)
@@ -46,7 +44,6 @@ class toString(unittest.TestCase):
 
         assert ''.join(string) == '20(8(4)(12(10)(14)))(22)'
 
-class TestTreesAndNodes(unittest.TestCase):
     def test_LCA_none_node(self):
         n1 = 1
         n2 = 3
@@ -77,6 +74,19 @@ class TestTreesAndNodes(unittest.TestCase):
         root = None
         assert lca(root, 3, 4) is None
 
+    def test_tree_out_of_order(self):
+        root = Node(50)
+        root.left = Node(30)
+        root.right = Node(10)
+        root.left.left = Node(40)
+        root.left.right = Node(70)
+        root.right.left = Node(20)
+        root.right.right = Node(60)
+
+        assert lca(root, 60, 40).data == 50
+        assert lca(root, 30, 40).data == 30
+        assert lca(root, 60, 20).data == 50
+        assert lca(root, 70, 20).data == 50
 
 
 
