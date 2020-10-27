42581a5cc5fd8c861a6fc49d37d35ba652bec37b
nacarney <nacarney@tcd.ie>
2020-10-27 15:30:09 +0000

    Figured out correct format, wrote test_lca_output case

--- LCA_Test.py
+++ LCA_Test.py
@@ -1,25 +1,40 @@
 import unittest
-from unittest import TestCase
+from LCA import Node
+from LCA import lca
+from LCA import treeToString
+
+class TestLCA(unittest.TestCase):
+    def test_lca_output(self):
+
+        root = Node(20)
+        root.left = Node(8)
+        root.right = Node(22)
+        root.left.left = Node(4)
+        root.left.right = Node(12)
+        root.left.right.left = Node(10)
+        root.left.right.right = Node(14)
+
+        n1 = 10
+        n2 = 14
+        assert lca(root, n1, n2).data == 12
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_lca_output_2(self):
+
+        root = Node(15)
+        root.left = Node(12)
+        root.left.left = Node(11)
+        root.left.right = Node(13)
+        root.right = Node(18)
+        root.right.left = Node(17)
+
+        n1 = 11
+        n2 = 13
+
+        #               _15_
+        #              /
+        #
+        #
+        #
 
 
-if __name__ == '__main__':
-    unittest.main()
-
-
-class Test(TestCase):
-    def test_lca(self):
-        self.fail()
-
-
-class TestNode(TestCase):
-    pass
-
-
-class Test(TestCase):
-    def test_tree_to_string(self):
-        self.fail()
